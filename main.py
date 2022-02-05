5ffd3469ebe42261ce06481b9c6f892d6827fe6d
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2022-02-05 19:52:23 +0200

    Lataa kysymykset netistä

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox
    
+from kysymykset import lataa_kysymykset_netista
 from quiz_ui import Ui_MainWindow
-
-KYSYMYKSET_JA_VASTAUKSET = [
-    (
-        "Mistä Python-ohjelmointikieli on saanut nimensä?", 
-        "käärmeestä",
-        "laulusta",
-        "*TV-sarjasta",
-        "elokuvasta",
-    ),
-    (
-        "Paljonko on 5*5?",
-        "22",
-        "*25",
-        "27",
-        "35",
-    ),
-]
 
 
 class MainWindow(QMainWindow):
@@ -26,12 +10,16 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.tiedot = lataa_kysymykset_netista()
         self.vaihda_kysymys_ja_vastaukset(0)
+        self.kytke_napit()
+        self.pisteet = 0
+        self.indeksi = 0
         
     def vaihda_kysymys_ja_vastaukset(self, indeksi):
-            tekstit = KYSYMYKSET_JA_VASTAUKSET[indeksi]
+            tekstit = self.tiedot[indeksi]
             uudet_tekstit = []
-            for (numero, teksti) in enumerate (tekstit):
+            for (numero, teksti) in enumerate(tekstit):
                 if teksti.startswith("*"):
                     teksti = teksti[1:]
                     self.oikea_vastaus = numero
@@ -39,9 +27,8 @@ class MainWindow(QMainWindow):
             self.aseta_tekstit(uudet_tekstit)
           
     def aseta_tekstit(self, tekstit):
-                self.aseta_kysymys(tekstit[0])
-                self.aseta_nappien_tekstit(tekstit[1:])
-        
+        self.aseta_kysymys(tekstit[0])
+        self.aseta_nappien_tekstit(tekstit[1:])
         
     def aseta_nappien_tekstit(self, tekstit):
         (t1, t2, t3, t4) = tekstit
@@ -52,8 +39,40 @@ class MainWindow(QMainWindow):
         
     def aseta_kysymys(self, kysymys):
         self.ui.label.setText(kysymys)
-
-
+        
+    def kytke_napit(self):
+        self.ui.button1.clicked.connect(self.nappia_painettu)       
+        self.ui.button2.clicked.connect(self.nappia_painettu)
+        self.ui.button3.clicked.connect(self.nappia_painettu)
+        self.ui.button4.clicked.connect(self.nappia_painettu)
+        
+    def nappia_painettu(self):
+        if self.sender() == self.ui.button1:
+            nappi = 1
+        elif self.sender() == self.ui.button2:
+            nappi = 2
+        elif self.sender() == self.ui.button3:
+            nappi = 3
+        elif self.sender() == self.ui.button4:
+            nappi = 4
+        else:
+            return
+        
+        if nappi == self.oikea_vastaus:
+            print("Oikein!")
+            self.pisteet += 1
+            
+        self.indeksi += 1
+        if self.indeksi >= len(self.tiedot):
+            laatikko = QMessageBox(self)
+            laatikko.setText(f"Peli  päättyi! Sait {self.pisteet} pistettä.")
+            laatikko.exec()
+            self.indeksi = 0
+            self.pisteet = 0
+        
+        self.vaihda_kysymys_ja_vastaukset(self.indeksi)
+        
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
